07d5e41f77066ca558906da6f0184b827778894c
Ayman Mohammad Alia <65292151+aymanmalawyer2@users.noreply.github.com>
2020-12-10 23:15:25 +0200

    Modify files names

---
+++ Assignment_001.py
@@ -0,0 +1,32 @@
+#-----------------------------------
+# This page created by Ayman and he uses many langueges.
+# It contains many functions to reach his goal fastly.
+#-----------------------------------
+
+
+myName = "Ayman"
+Age = 36
+my_Country = "Syria"
+
+
+print(type("myName"))
+print(type(Age))
+print(type("my_Country"))
+
+print("#"*20)
+
+# This is string
+myName = "Ayman"
+# This is integer
+Age = 36
+# This is string
+my_Country = "Syria"
+
+# I can't concatenate integer with string because Python will give me an error
+print("Hello + " " + ("myName") + " " + Your Age Is + " " + ("Age") + " " + & Country Is ("my_Country")")
+
+# I can't concatenate int with str because Python will give me an error
+myInfo = myName + " " + Age + " " + my_Country
+print(myInfo)
+
+
